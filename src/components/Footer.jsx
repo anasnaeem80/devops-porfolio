51b5c95953b9fb0a215705592d425c934d6d2aa4
Muhammad Anas Naeem <anas.naeem.998@gmail.com>
2026-02-19 00:33:54 +0500

    footer fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,11 @@ const Footer = () => {
   return (
     <footer className='bg-dark text-white py-8'>
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center'>
-        <p className='text-gray-400 flex items-center justify-center'>
+        {/* <p className='text-gray-400 flex items-center justify-center'>
           © {new Date().getFullYear()} Muhammad Anas Naeem. Built with
           <Heart className='w-4 h-4 mx-1 text-red-500 fill-current' />
           using React & Tailwind CSS
-        </p>
+        </p> */}
 
         <div className='mt-4'>
           <a
